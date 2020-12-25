3f1dc62379a5838ea91c3e7d71cfc29ab60f2eb5
Joose Sainio <joose.sainio@student.tut.fi>
2020-12-25 18:52:29 +0200

    Start day 23 part 2, doesn't work yet

--- src/day23.py
+++ src/day23.py
@@ -2,6 +2,7 @@ class Cups:
 
     def __init__(self, d):
         self.cups = list(int(x) for x in d)
+        # self.cups.extend(range(10, 1000001))
         self.current = self.cups[0]
         self._destination = self.cups[0]
         self.picked_up = []
@@ -12,17 +13,35 @@ class Cups:
 
     @destination.setter
     def destination(self, value):
-        assert 1 <= value <= 9
+        assert 1 <= value <= len(self.cups)
         self._destination = value
 
     def move(self):
-        temp = []
-        for i in self.cups:
-            if i not in self.picked_up:
-                temp.append(i)
-            if i == self.destination:
-                temp.extend(self.picked_up)
-
+        pick_up_index = self.cups.index(self.current) + 1
+        destination_index = self.cups.index(self._destination)
+        if pick_up_index < destination_index:
+            if pick_up_index + 3 >= len(self.cups):
+                temp = self.picked_up[len(self.cups) - pick_up_index:]
+            temp = self.cups[0:pick_up_index]
+            temp.extend(
+                self.cups[pick_up_index + 4:destination_index + 1]
+            )
+            temp.extend(
+                self.picked_up
+            )
+            temp.extend(
+                self.cups[destination_index + 1:]
+            )
+            temp.extend(self.picked_up)
+        else:
+            temp = self.cups[0:destination_index + 1]
+            temp.extend(
+                self.cups[destination_index + 1: pick_up_index]
+            )
+            temp.extend(
+                self.cups[pick_up_index + 4:]
+            )
+        print(temp)
         self.cups = temp
         self.current = self.cups[(self.cups.index(self.current) + 1) % 9]
 
@@ -30,14 +49,14 @@ class Cups:
         cups_index = self.cups.index(self.current)
         self.picked_up = []
         for i in range(cups_index + 1, cups_index + 4):
-            self.picked_up.append(self.cups[i % 9])
+            self.picked_up.append(self.cups[i % len(self.cups)])
 
         new_dest = self.current - 1 if self.current != 1 else 9
 
         while new_dest in self.picked_up or new_dest == self.current:
             new_dest = new_dest - 1
             if new_dest == 0:
-                new_dest = 9
+                new_dest = len(self.cups)
 
         self.destination = new_dest
 
@@ -45,14 +64,16 @@ class Cups:
     def answer(self):
         k = ""
         start = self.cups.index(1)
-        for i in range(1, 9):
-            k += str(self.cups[(i + start) % 9])
+        for i in range(1, 2):
+            k += str(self.cups[(i + start) % len(self.cups)])
         return k
 
 
 def first():
     cups = Cups("193467258")
-    for _ in range(100):
+    for i in range(100):
+        if (i + 1) % 10000 == 0:
+            print("*", end="")
         cups.pick_up()
         cups.move()
     print(cups.answer)
